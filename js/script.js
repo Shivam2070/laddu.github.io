d24d4ffa37755e0a27b78588a3fdfa1759834c4e
Shivam2070 <67461594+Shivam2070@users.noreply.github.com>
2023-01-11 13:08:08 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function checkPassword() {
     // Replace 'correctPassword' with the actual password
-    if (document.getElementById('password').value === '123') {
+    if (document.getElementById('password').value === '@HelloWorl') {
       window.location.href = 'main.html';
       return false;
     } else {
-      alert('Incorrect password');
+      alert('Incorrect Code');
       return false;
     }
   }
